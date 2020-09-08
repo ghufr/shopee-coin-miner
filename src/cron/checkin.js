77e1675d696ef5c4887d526236852132eaade716
ghufr <ghufronfr@gmail.com>
2020-09-08 10:33:19 +0700

    Add refresh token mechanism

--- src/cron/checkin.js
+++ src/cron/checkin.js
@@ -1,19 +1,29 @@
 require("dotenv").config();
 
 const coins = require("../packages/coins");
+const account = require("../packages/account");
+const logger = require("../utils/logger");
 
-const token = process.env.SHOPEE_TOKEN;
+// const token = process.env.SHOPEE_TOKEN;
 
-// Every 00:00 am
+// 0 1 * * *
+//  5, 10,  15, 20, 25, 30, 150 = 255/w = 1020/m
+
+const shopeeToken = process.env.SHOPEE_TOKEN_COOKIE;
+
 (async () => {
   try {
+    const Ua = "Shopee Android Beeshop locale/id version=376 appver=26008";
+    const token = await account.refresh({ shopeeToken, Ua });
+
     const checkin = await coins.checkin({ token });
-    console.log(checkin);
+
     if (checkin.code === 0 && checkin.data.success) {
-      console.log(checkin);
-      console.log(
-        `Selamat anda mendapatkan ${checkin.data.increase_coins} koin`
-      );
+      logger.log({
+        level: "info",
+        message: checkin.data.increase_coins + " koin",
+      });
+      console.log(`+ Anda mendapatkan ${checkin.data.increase_coins} koin`);
     }
   } catch (err) {
     console.log(err);
--- src/cron/luckydraw.js
+++ src/cron/luckydraw.js
@@ -1,18 +1,27 @@
 require("dotenv").config();
 
 const luckydraw = require("../packages/luckydraw");
+const account = require("../packages/account");
 
-const token = process.env.SHOPEE_TOKEN;
+const shopeeToken = process.env.SHOPEE_TOKEN_COOKIE;
 
+// 0 9 * * *
 (async () => {
   try {
-    const prize = await luckydraw.claim({
-      token,
-      id: "ff7d6916be64b8b4",
-    });
-    console.log(prize);
-    if (prize.code === 0) {
-      // console.log(`Selamat anda mendapatkan ${water.increase_coins} koin`);
+    const Ua = "Shopee Android Beeshop locale/id version=376 appver=26008";
+    const token = await account.refresh({ shopeeToken, Ua });
+
+    // Cek token
+    const chances = await luckydraw.chances({ token });
+    if (chances.chance_count > 0) {
+      // Claim hadiah
+      const prize = await luckydraw.claim({
+        token,
+        id: "ff7d6916be64b8b4",
+      });
+      if (prize.award.award_value_float > 1) {
+        console.log(`+ Anda mendapatkan ${prize.award.award_value_float} koin`);
+      }
     }
   } catch (err) {
     console.log(err);
