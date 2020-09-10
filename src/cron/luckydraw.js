54b95154b3c64d427182846ebe6ff91bd3c4cd06
ghufr <ghufronfr@gmail.com>
2020-09-10 22:04:21 +0700

    add credentials

--- src/cron/luckydraw.js
+++ src/cron/luckydraw.js
@@ -4,28 +4,35 @@ const luckydraw = require("../packages/luckydraw");
 const account = require("../packages/account");
 const logger = require("../utils/logger");
 
-const shopeeToken = process.env.SHOPEE_TOKEN_COOKIE;
+const fs = require("fs");
 
 // 0 9 * * *
 (async () => {
   try {
-    const Ua = "Shopee Android Beeshop locale/id version=376 appver=26008";
-    const token = await account.refresh({ shopeeToken, Ua });
+    const raw = fs.readFileSync("./credentials.json");
+    const credentials = JSON.parse(raw);
 
-    // Cek token
-    const chances = await luckydraw.chances({ token });
-    if (chances.chance_count > 0) {
-      // Claim hadiah
-      const prize = await luckydraw.claim({
-        token,
-        id: "ff7d6916be64b8b4",
-      });
-      if (prize.award.award_value_float > 1) {
-        logger.log({
-          level: "info",
-          message: `+ ${prize.award.award_value_float} koin`,
+    for (let i = 0; i < credentials.length; i++) {
+      const { shopeeToken, name } = credentials[i];
+      const Ua = "Shopee Android Beeshop locale/id version=376 appver=26008";
+      const token = await account.refresh({ shopeeToken, Ua });
+
+      // Cek token
+      const chances = await luckydraw.chances({ token });
+      if (chances.chance_count > 0) {
+        // Claim hadiah
+        const prize = await luckydraw.claim({
+          token,
+          id: "ff7d6916be64b8b4",
         });
-        console.log(`+ Anda mendapatkan ${prize.award.award_value_float} koin`);
+        if (prize.award.award_value_float > 1) {
+          const message = `${name} mendapatkan ${prize.award.award_value_float} koin`;
+          logger.log({
+            level: "info",
+            message,
+          });
+          console.log(`+ ${message}`);
+        }
       }
     }
   } catch (err) {
