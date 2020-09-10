3838c548b985b63597c944e976cbb6cd71af019e
ghufr <ghufronfr@gmail.com>
2020-09-10 20:27:39 +0700

    use credentials

--- src/cron/checkin.js
+++ src/cron/checkin.js
@@ -4,26 +4,41 @@ const coins = require("../packages/coins");
 const account = require("../packages/account");
 const logger = require("../utils/logger");
 
+const fs = require("fs");
+
 // const token = process.env.SHOPEE_TOKEN;
 
 // 0 1 * * *
 //  5, 10,  15, 20, 25, 30, 150 = 255/w = 1020/m
 
-const shopeeToken = process.env.SHOPEE_TOKEN_COOKIE;
-
 (async () => {
   try {
-    const Ua = "Shopee Android Beeshop locale/id version=376 appver=26008";
-    const token = await account.refresh({ shopeeToken, Ua });
+    const raw = fs.readFileSync("./credentials.json");
+    const credentials = JSON.parse(raw);
 
-    const checkin = await coins.checkin({ token });
+    for (let i = 0; i < credentials.length; i++) {
+      const { shopeeToken, name } = credentials[i];
 
-    if (checkin.code === 0 && checkin.data.success) {
-      logger.log({
-        level: "info",
-        message: checkin.data.increase_coins + " koin",
+      const Ua = "Shopee Android Beeshop locale/id version=376 appver=26008";
+      const token = await account.refresh({
+        shopeeToken,
+        Ua,
       });
-      console.log(`+ Anda mendapatkan ${checkin.data.increase_coins} koin`);
+
+      const checkin = await coins.checkin({ token });
+
+      // console.log(checkin);
+
+      if (checkin.code === 0 && checkin.data.success) {
+        logger.log({
+          level: "info",
+          message: `${name} mendapatkan ${checkin.data.increase_coins} koin`,
+          label: "checkin",
+        });
+        console.log(
+          `+ ${name} mendapatkan ${checkin.data.increase_coins} koin`
+        );
+      }
     }
   } catch (err) {
     console.log(err);
