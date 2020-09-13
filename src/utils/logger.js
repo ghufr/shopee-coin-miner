21c0714d79079a8cc79bcc03b604575fb75abf06
ghufr <ghufronfr@gmail.com>
2020-09-13 18:00:28 +0700

    logger file

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -3,7 +3,7 @@ const { createLogger, format, transports } = require("winston");
 const logger = createLogger({
   format: format.combine(format.timestamp(), format.json()),
   transports: [
-    new transports.File({ filename: "actions.log", level: "info" }),
+    new transports.File({ filename: "./logs/actions.log", level: "info" }),
     new transports.Console(),
   ],
 });
