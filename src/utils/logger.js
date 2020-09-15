a4b603e76823ab9af898438f1b77f31124d060c4
ghufr <ghufronfr@gmail.com>
2020-09-15 09:24:14 +0700

    add error logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -4,6 +4,7 @@ const logger = createLogger({
   format: format.combine(format.timestamp(), format.json()),
   transports: [
     new transports.File({ filename: "./logs/actions.log", level: "info" }),
+    new transports.File({ filename: "./logs/errors.log", level: "error" }),
     new transports.Console(),
   ],
 });
