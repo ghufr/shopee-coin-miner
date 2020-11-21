33f2d01dc4b43fe3ca214fa2a9ba24968b689ac6
ghufr <ghufronfr@gmail.com>
2020-11-21 19:10:18 +0700

    Add log rotate

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,10 +1,23 @@
 const { createLogger, format, transports } = require("winston");
+const winston = require("winston/lib/winston/config");
+require("winston-daily-rotate-file");
 
 const logger = createLogger({
   format: format.combine(format.timestamp(), format.json()),
   transports: [
-    new transports.File({ filename: "./logs/actions.log", level: "info" }),
-    new transports.File({ filename: "./logs/errors.log", level: "error" }),
+    new transports.DailyRotateFile({
+      filename: "./logs/actions.log",
+      level: "info",
+      frequency: "24h",
+      datePattern: "YYYY-MM-DD",
+      zippedArchive: true,
+      maxSize: "10m",
+      maxFiles: "14d",
+    }),
+    new transports.DailyRotateFile({
+      filename: "./logs/errors.log",
+      level: "error",
+    }),
     new transports.Console(),
   ],
 });
